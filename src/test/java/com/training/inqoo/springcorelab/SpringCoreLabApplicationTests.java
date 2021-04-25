b4bd402097c032967876585e3fca9995c733f433
mb83pl <mbutora@gmail.com>
2021-04-25 09:05:14 +0200

    init

---
+++ src/test/java/com/training/inqoo/springcorelab/SpringCoreLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.inqoo.springcorelab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
